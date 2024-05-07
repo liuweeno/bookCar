24aa25368df8382b0ceaaea1ba96d20e95fce018
liuween <q1612487215@163.com>
2024-05-08 00:35:14 +0800

    fix: change the request base url

--- src/api/request.js
+++ src/api/request.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 import { getToken } from '@/utils/token';
 
 const request = axios.create({
-  baseURL: 'http://101.37.87.187:8081/',
+  // baseURL: 'http://101.37.87.187:8081/',
   //baseURL: 'http://39.99.234.229:9508/api',
+  //baseURL: 'http://127.0.0.1:8081/',
   timeout: 2000,
 });
 
